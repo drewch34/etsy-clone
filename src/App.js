0a5043c9ae4efa656609dddad233990e61c26ea1
Talles Corradini <tallescorradini@gmail.com>
2022-02-07 11:11:45 -0300

    feat: add page header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { makeServer } from "./mockServer";
+import styles from "./styles/Home.module.scss";
+import logo from "./assets/logo.svg";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+import { CategoryNavigation } from "./components/CategoryNavigation/CategoryNavigation";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
+}
+
 function App() {
-  return <div>Etsy-clone</div>;
+  return (
+    <div>
+      <header className={styles.header}>
+        <a href="/#" tabIndex={0} className={styles.logo}>
+          <img src={logo} alt="Logo" />
+        </a>
+
+        <nav aria-label="Main navigation" className={styles.mainNavigation}>
+          <ul className={styles.links}>
+            <li>
+              <a href="/#" className={styles.signInLink}>
+                Sign in
+              </a>
+            </li>
+            <li>
+              <a
+                href="/#"
+                aria-label="Shopping cart"
+                className={styles.cartLink}
+              >
+                <span aria-hidden="true" className="icon lg cart"></span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <CategoryNavigation className={styles.categoryNavigation} />
+
+        <SearchBar className={styles.searchBar} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
