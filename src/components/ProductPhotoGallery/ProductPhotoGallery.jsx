dd773143e1a2b8230d88d96b80038256e7aefc77
Drew <firefan34@gmail.com>
2022-02-19 10:03:09 -0500

    some photo functionality

--- src/components/ProductPhotoGallery/ProductPhotoGallery.jsx
+++ src/components/ProductPhotoGallery/ProductPhotoGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./ProductPhotoGallery.module.scss";
 import { images } from "../PhotoData";
 
@@ -6,10 +6,39 @@ import { images } from "../PhotoData";
 export function ProductPhotoGallery({className}) {
 
   const [currImage, setImage] = useState(images[0]);
+  const [currImageIndx, setImageIndx] = useState(0);
 
-  function selectImage() {
-    setImage()
-  }
+  useEffect(() => {
+    if (images && images[0]) {
+      setImage(images[0]);
+      setImageIndx(0);
+    }
+  });
+
+  const handleSelectedImageChange = (newIndx: number) => {
+    if (images && images.length > 0) {
+      setImage(images[newIndx]);
+      setImageIndx(newIndx);
+      if (images?.current[newIndx]) {
+        images?.current[newIndx]?.scrollIntoView({
+          inline: "center",
+          behavior: "smooth"
+        });
+      }
+    }
+  };
+
+  function handleLeftClick() {
+    if(images && images.length > 0) {
+      let newIndx = currImageIndx - 1;
+      if (newIndx < 0) {
+        newIndx = images.length - 1;
+      }
+      handleSelectedImageChange(newIndx);
+    }
+  };
+
+
 
   return (
     <div className={`${styles.photo} ${className}`}>
@@ -23,9 +52,18 @@ export function ProductPhotoGallery({className}) {
         </ol>
       </div>
 
-      <div>
-        <img src={currImage} className={styles.currImage}></img>
+      <div className={styles.buttoncont}>
+        <button className={`${styles.carouselbtn} ${styles.carselbtnleft}`} onClick={handleLeftClick}>
+          lft
+        </button>
+
+      </div>
+
+      <div className={styles.currImageCont}>
+        <img className={styles.currImage} src={currImage} alt=""></img>
       </div>
     </div>
   );
+
 }
+
