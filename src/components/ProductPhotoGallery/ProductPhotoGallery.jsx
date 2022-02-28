3814b5a027286675f579c31f1c938f43d16581a4
Drew <firefan34@gmail.com>
2022-02-28 15:54:21 -0500

    carousel function

--- src/components/ProductPhotoGallery/ProductPhotoGallery.jsx
+++ src/components/ProductPhotoGallery/ProductPhotoGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from "./ProductPhotoGallery.module.scss";
 import { images } from "../PhotoData";
 import { arrows } from "../PhotoButtons";
@@ -8,23 +8,13 @@ export function ProductPhotoGallery({className}) {
   const [currImage, setImage] = useState(images[0]);
   const [currImageIndx, setImageIndx] = useState(0);
 
-  useEffect(() => {
-    if (images && images[0]) {
-      setImage(images[0]);
-      setImageIndx(0);
-    }
-  });
+
+
 
   const handleSelectedImageChange = (newIndx: number) => {
     if (images && images.length > 0) {
       setImage(images[newIndx]);
       setImageIndx(newIndx);
-      if (images?.current[newIndx]) {
-        images?.current[newIndx]?.scrollIntoView({
-          inline: "center",
-          behavior: "smooth"
-        });
-      }
     }
   };
 
@@ -33,6 +23,16 @@ export function ProductPhotoGallery({className}) {
       let newIndx = currImageIndx - 1;
       if (newIndx < 0) {
         newIndx = images.length - 1;
+      }
+      handleSelectedImageChange(newIndx);
+    }
+  };
+
+  function handleRghtClick() {
+    if(images && images.length > 0) {
+      let newIndx = currImageIndx + 1;
+      if (newIndx >= images.length) {
+        newIndx = 0;
       }
       handleSelectedImageChange(newIndx);
     }
@@ -53,13 +53,19 @@ export function ProductPhotoGallery({className}) {
 
       <div className={styles.buttoncont}>
         <button  className={`${styles.carouselbtn} ${styles.carselbtnleft}`} >
-          <image src={arrows[0]} onClick={handleLeftClick}/>
+          <img className={styles.butnimg} src={arrows[0]} alt="" onClick={handleLeftClick}></img>
         </button>
 
       </div>
 
       <div className={styles.currImageCont}>
         <img className={styles.currImage} src={currImage} alt=""></img>
+      </div>
+
+      <div className={styles.buttoncontrght}>
+        <button className={`${styles.carouselbtnrght}`}>
+          <img className={styles.butnimg} src={arrows[1]} alt="" onClick={handleRghtClick}></img>
+        </button>
       </div>
     </div>
   );
