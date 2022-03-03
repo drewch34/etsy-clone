f17f535a5322f409145124c6e00108c256217796
Drew <firefan34@gmail.com>
2022-03-03 18:53:36 -0500

    selection functionality

--- src/components/ProductPhotoGallery/ProductPhotoGallery.jsx
+++ src/components/ProductPhotoGallery/ProductPhotoGallery.jsx
@@ -16,6 +16,8 @@ export function ProductPhotoGallery({className}) {
       setImage(images[newIndx]);
       setImageIndx(newIndx);
     }
+
+    console.log(newIndx);
   };
 
   function handleLeftClick() {
@@ -42,27 +44,27 @@ export function ProductPhotoGallery({className}) {
 
   return (
     <div className={`${styles.photo} ${className}`}>
-      <div className={styles.images}>
+      <div className={`${styles.images} ${styles.sub}`}>
         <div>
-          {images.map((image) => (
-            <div key={image} style={{ backgroundImage:`url(${image})`}} className={styles.image}>
+          {images.map((image, index) => (
+            <div key={index} style={{ backgroundImage:`url(${image})`}} className={styles.image} onClick={(e)=> handleSelectedImageChange(index)}>
             </div>
           ))}
         </div>
       </div>
 
-      <div className={styles.buttoncont}>
+      <div className={`${styles.buttoncont} ${styles.sub}`}>
         <button  className={`${styles.carouselbtn} ${styles.carselbtnleft}`} >
           <img className={styles.butnimg} src={arrows[0]} alt="" onClick={handleLeftClick}></img>
         </button>
 
       </div>
 
-      <div className={styles.currImageCont}>
+      <div className={`${styles.currImageCont} ${styles.sub}`}>
         <img className={styles.currImage} src={currImage} alt=""></img>
       </div>
 
-      <div className={styles.buttoncontrght}>
+      <div className={`${styles.buttoncontrght} ${styles.sub}`}>
         <button className={`${styles.carouselbtnrght}`}>
           <img className={styles.butnimg} src={arrows[1]} alt="" onClick={handleRghtClick}></img>
         </button>
